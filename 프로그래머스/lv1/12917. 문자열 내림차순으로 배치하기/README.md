e21205d702533fe63a1f288cf26b19706d1c7a2b
DongHyeon Kim (Ramaste) <98330884+DhKimy@users.noreply.github.com>
2022-12-25 14:55:53 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.09 ms, Memory: 16 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/README.md
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내림차순으로 배치하기 - 12917 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12917) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12917#) 
 
 ### 성능 요약
 
-메모리: 16.2 MB, 시간: 0.14 ms
+메모리: 16 MB, 시간: 0.09 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.swift
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.swift
@@ -1,13 +1,3 @@
 func solution(_ s:String) -> String {
-    var lowerArray = [Character]()
-    var upperArray = [Character]()
-    for i in Array(s) {
-        if i.isLowercase {
-            lowerArray.append(i)
-        } else {
-            upperArray.append(i)
-        }
-    }
-    let answer = [String(lowerArray.sorted(by: >)), String(upperArray.sorted(by: >))]
-    return answer.joined()
+    String(s.sorted(by: { $0 > $1 }))
 }
