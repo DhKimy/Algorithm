a2672553bc3ea9f8bdd1b8fa9dc1cae008c40ae2
DongHyeon Kim (Ramaste) <98330884+DhKimy@users.noreply.github.com>
2022-12-25 14:43:43 +0900

    [level 1] Title: 내적, Time: 0.39 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.swift
+++ 프로그래머스/lv1/70128. 내적/내적.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ a:[Int], _ b:[Int]) -> Int {
-    var sum = 0
-    for i in 0 ... a.count - 1 {
-        sum += a[i] * b[i]
-    }
-    return sum
+    return (0 ... a.count - 1).map({ a[$0] * b[$0] }).reduce(0, +)
 }
