20b87caa544bd58117ac40b44eb897465e4625bb
DongHyeon Kim (Ramaste) <98330884+DhKimy@users.noreply.github.com>
2022-12-26 23:45:20 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 8.51 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.swift
@@ -0,0 +1,12 @@
+func solution(_ strings:[String], _ n:Int) -> [String] {
+    
+    var arrayStrings = strings
+
+    
+    
+    return arrayStrings.sorted().sorted(by: { sortAESC($0, $1, n) })
+}
+
+func sortAESC(_ num1: String, _ num2: String, _ n: Int) -> Bool {
+   return Array(num1)[n] < Array(num2)[n]
+}
