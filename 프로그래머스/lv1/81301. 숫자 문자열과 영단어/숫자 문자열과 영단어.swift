29c46cb6d80d1b9a510a22bcdf5873845a38135f
DongHyeon Kim (Ramaste) <98330884+DhKimy@users.noreply.github.com>
2022-12-27 17:25:29 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.11 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ s:String) -> Int {
+    var innerS = s
+    let dict: [String: String] = ["zero" : "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"]
+    
+    for (key, value) in dict {
+        innerS = innerS.replacingOccurrences(of: key, with: value)
+    }
+    return Int(innerS)!
+}
