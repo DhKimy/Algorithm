102b1f09a4040dbb9f6a2787b47efc203b8a6fd8
DongHyeon Kim (Ramaste) <98330884+DhKimy@users.noreply.github.com>
2023-01-03 10:17:58 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func solution(_ ingredient: [Int]) -> Int {
+    var stack: [Int] = []
+    var answer = 0
+    
+    for i in ingredient {
+        
+        stack.append(i)
+        
+        if stack.count < 4 { continue }
+        
+        let n = stack.count
+        if Array(stack[n - 4 ..< n]) == [1, 2, 3, 1] {
+            stack = Array(stack[0 ..< n - 4])
+            answer += 1
+        }
+        
+    }
+    return answer
+}
