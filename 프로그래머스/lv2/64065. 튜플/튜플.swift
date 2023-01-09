5ff78118f7db30a61a564fe86977e9bdcf78b77b
DongHyeon Kim (Ramaste) <98330884+DhKimy@users.noreply.github.com>
2023-01-09 18:13:25 +0900

    [level 2] Title: 튜플, Time: 0.20 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/64065. 튜플/튜플.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+func solution(_ s:String) -> [Int] {
+    var answer = [Int]()
+    var tuple = [[Int]]()
+    var tempArray = [Int]()
+    var tempString: String = ""
+    for (index, i) in s.enumerated() {
+        if i == "," {
+            tempArray.append(Int(tempString)!)
+            tempString = ""
+        } else if i.isNumber {
+            tempString += String(i)
+        } else if i == "{" {
+            if !(tempArray.isEmpty) {
+                tuple.append(tempArray)
+                tempArray = [Int]()
+            }
+        } else {
+            if index == s.count - 2 {
+                tempArray.append(Int(tempString)!)
+                tempString = ""
+            } else if index == s.count - 1{
+                tuple.append(tempArray)
+            }
+        }
+    }
+    tuple.sort(by: { $0.count < $1.count })
+    for i in 0 ... tuple.count - 1 {
+        let temp1 = Set(answer)
+        let temp2 = Set(tuple[i])
+        let resultNumberSet: [Int] = Array(temp2.subtracting(temp1))
+        answer.append(resultNumberSet[0])
+    }
+    return answer
+}
