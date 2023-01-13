70eb36aa96c21913b1a502c754d37ee8f3c167a1
DongHyeon Kim (Ramaste) <98330884+DhKimy@users.noreply.github.com>
2023-01-13 14:06:26 +0900

    [level 2] Title: 할인 행사, Time: 0.08 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+func solution(_ want:[String], _ number:[Int], _ discount:[String]) -> Int {
+    var answer = 0
+    var wantList: Dictionary = [String : Int]()
+    initWantList(&wantList, want, number)
+    
+    for i in 0 ... discount.count - number.reduce(0, +) {
+        for j in i ..< i + 10 {
+            if wantList.contains(where: { $0.key == discount[j] }) {
+                wantList[discount[j]]! -= 1
+            } else {
+                break
+            }
+            if wantList[discount[j]]! < 0 {
+                break
+            }
+            if j == i + 9 && wantList.map({ (key: String, value: Int) -> Int in return value}).reduce(0, +) == 0 {
+                answer += 1
+            }
+        }
+        initWantList(&wantList, want, number)
+    }
+    return answer
+}
+
+func initWantList(_ wantList : inout [String : Int], _ want: [String], _ number: [Int]) {
+    for (index, i) in want.enumerated() {
+        wantList[i] = number[index]
+    }
+}
