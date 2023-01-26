5c5bdd8d5208eb6927a45fd4c7d5da6105af30fe
DongHyeon Kim (Ramaste) <98330884+DhKimy@users.noreply.github.com>
2023-01-26 14:47:34 +0900

    [level 2] Title: 짝지어 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.swift
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.swift
@@ -4,12 +4,11 @@ func solution(_ s:String) -> Int{
     let arrayS = Array(s)
     var stack = [Character]()
     for i in arrayS {
-        stack.append(i)
-        let temp = stack.count
-        if temp >= 2 && stack[temp - 1] == stack[temp - 2] {
+        if stack.last ?? "0" == i {
             stack.removeLast()
-            stack.removeLast()
+            continue
         }
+        stack.append(i)
     }
     return stack.count == 0 ? 1 : 0
 }
