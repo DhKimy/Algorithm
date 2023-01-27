f80367a987fdd4fed7e00ccf09a139429c093be8
DongHyeon Kim (Ramaste) <98330884+DhKimy@users.noreply.github.com>
2023-01-27 19:59:25 +0900

    [level 2] Title: [3차] n진수 게임, Time: 5.07 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.swift
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.swift
@@ -4,7 +4,6 @@ func solution(_ n:Int, _ t:Int, _ m:Int, _ p:Int) -> String {
         let temp = String(i, radix: n).uppercased()
         numbers += temp
     }
-    
     let newNumbers = Array(numbers)
     var answer: String = ""
     for i in stride(from: p - 1, to: p - 1 + m * t, by: m) {
