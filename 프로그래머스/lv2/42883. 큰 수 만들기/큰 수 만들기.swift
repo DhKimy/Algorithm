d0a36dda6848bd24dc4937dbdcf82b80791a70ef
DongHyeon Kim (Ramaste) <98330884+DhKimy@users.noreply.github.com>
2023-02-03 09:03:56 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+func solution(_ number:String, _ k:Int) -> String {
+    var numbers = Array(number)
+    var stack = [Character]()
+    var count = 0
+    var i = 0
+    
+    while count < k && i < numbers.count {
+        guard let last = stack.last else {
+            stack.append(numbers[i])
+            i += 1
+            continue
+        }
+        
+        if last < numbers[i] {
+            stack.removeLast()
+            count += 1
+            continue
+        }
+        
+        stack.append(numbers[i])
+        i += 1
+    }
+    
+    if count >= k {
+        stack.append(contentsOf: numbers[i...])
+    }
+    if i >= numbers.count {
+        return stack[0..<numbers.count - k].reduce(""){"\($0)\($1)"}
+    }
+    return stack.reduce(""){"\($0)\($1)"}
+}
