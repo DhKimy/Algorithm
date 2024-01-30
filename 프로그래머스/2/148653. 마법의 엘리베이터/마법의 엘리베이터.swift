d70d7d0ce2ff05f551633118e3b1eb75153e54f8
DongHyeon Kim(Duncan) <98330884+DhKimy@users.noreply.github.com>
2024-01-31 01:08:43 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.00 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ storey: Int) -> Int {
+    var storey = storey
+    var result = 0
+
+    while storey != 0 {
+    	// 상승
+        if storey % 10 > 5 || (storey % 10 == 5 && (storey / 10) % 10 >= 5) {
+            result += 10 - (storey % 10)
+            // 앞자리 + 1
+            storey = (storey / 10) + 1
+        } else {
+        // 하강
+            result += storey % 10
+            storey = storey / 10
+        }
+    }
+    return result
+}
