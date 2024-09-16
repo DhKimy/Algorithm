61dacabdb6a9b660522088cd9a06847b41bbb3de
DongHyeon Kim(Duncan) <98330884+DhKimy@users.noreply.github.com>
2024-09-16 13:57:31 +0900

    [Silver I] Title: 숨바꼭질, Time: 12 ms, Memory: 72376 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.swift
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.swift
@@ -1,6 +1,4 @@
-solution_1697()
-
-func solution_1697() {
+func boj_1697() {
     let input = readLine()!.split(separator: " ").map { Int($0)! }
     let (n, k) = (input[0], input[1])
 
@@ -9,41 +7,30 @@ func solution_1697() {
         return
     }
 
-    var visited = Array(repeating: false, count: 100001)
-    var queue = [(position: Int, time: Int)]()
-    queue.append((n, 0))
+    var visited = [Bool](repeating: false, count: 100001)
+    var queue = [(n, 0)] // (현재 위치, 이동 횟수)
+    var index = 0
+
     visited[n] = true
 
-    let directions = [-1, 1]
+    while index < queue.count {
+        let (current, time) = queue[index]
+        index += 1
 
-    var front = 0
-
-    while front < queue.count {
-        let (currentPosition, currentTime) = queue[front]
-        front += 1
-
-        let nextTime = currentTime + 1
-
-        for direction in directions {
-            let nextPosition = currentPosition + direction
-            if nextPosition == k {
-                print(nextTime)
-                return
-            }
-            if nextPosition >= 0 && nextPosition <= 100000 && !visited[nextPosition] {
-                visited[nextPosition] = true
-                queue.append((nextPosition, nextTime))
-            }
-        }
-
-        let teleportPosition = currentPosition * 2
-        if teleportPosition == k {
-            print(nextTime)
+        if current == k {
+            print(time)
             return
         }
-        if teleportPosition <= 100000 && !visited[teleportPosition] {
-            visited[teleportPosition] = true
-            queue.append((teleportPosition, nextTime))
+
+        let nextPositions = [current - 1, current + 1, current * 2]
+
+        for next in nextPositions {
+            if next >= 0 && next < 100001 && !visited[next] {
+                visited[next] = true
+                queue.append((next, time + 1))
+            }
         }
     }
 }
+
+boj_1697()
