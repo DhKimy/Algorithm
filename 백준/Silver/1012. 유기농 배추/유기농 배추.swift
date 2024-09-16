12788c4f79b531e97ffe253f0f1bb6ea9cb7861f
DongHyeon Kim(Duncan) <98330884+DhKimy@users.noreply.github.com>
2024-09-16 12:33:23 +0900

    [Silver II] Title: 유기농 배추, Time: 12 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.swift
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.swift
@@ -1,59 +1,56 @@
-solution_1012_solve()
-
-func solution_1012_solve() {
+func boj_1012() {
     let t = Int(readLine()!)!
 
     for _ in 0..<t {
-        solution_1012()
-    }
-}
+        let input = readLine()!.split(separator: " ").map { Int($0)! }
+        let (m, n, k) = (input[0], input[1], input[2])
+        var cabbagePos = [[Int]](repeating: [Int](repeating: 0, count: m), count: n)
+        var visited = [[Bool]](repeating: [Bool](repeating: false, count: m), count: n)
+        var queue = [(Int, Int)]()
+        var bugCount = 0
 
-private func solution_1012() {
-    let input = readLine()!.split(separator: " ").compactMap { Int($0) }
-    let (m, n, k) = (input[0], input[1], input[2]) // 가로, 세로, 배추 개수
-    var graph = [[Int]](repeating: [Int](repeating: 0, count: m), count: n)
-    var visited = [[Bool]](repeating: [Bool](repeating: false, count: m), count: n)
-    var queue = [(x: Int, y: Int)]()
-    var waiting = [(x: Int, y: Int)]()
-    var answer = 0
-    let directions = [(0, -1), (0, 1), (-1, 0), (1, 0)] // 상하좌우
-    for _ in 0..<k {
-        let pos = readLine()!.split(separator: " ").compactMap { Int($0) }
-        graph[pos[1]][pos[0]] = 1
-        waiting.append((pos[0], pos[1]))
-    }
-
-    while !waiting.isEmpty {
-        let first = waiting.removeFirst()
-
-        if visited[first.y][first.x] {
-            continue
-        } else {
-            queue.append((first.x, first.y))
-            visited[first.y][first.x] = true
-            bfs()
-            answer += 1
+        // 배추 위치 저장
+        for _ in 0..<k {
+            let pos = readLine()!.split(separator: " ").map { Int($0)! }
+            cabbagePos[pos[1]][pos[0]] = 1
         }
+
+        // BFS 탐색
+        for y in 0..<n {
+            for x in 0..<m {
+                if cabbagePos[y][x] == 1 && !visited[y][x] {
+                    bugCount += 1
+                    bfs(startX: x, startY: y, &cabbagePos, &visited, &queue)
+                }
+            }
+        }
+
+        print(bugCount)
     }
 
-    print(answer)
+    func bfs(startX: Int, startY: Int, _ cabbagePos: inout [[Int]], _ visited: inout [[Bool]], _ queue: inout [(Int, Int)]) {
+        let dx = [-1, 1, 0, 0]
+        let dy = [0, 0, -1, 1]
 
-    func bfs() {
+        queue.append((startX, startY))
+        visited[startY][startX] = true
+
         while !queue.isEmpty {
-            let first = queue.removeFirst()
+            let (x, y) = queue.removeFirst()
 
-            for direction in directions {
-                let movedX = first.x + direction.0
-                let movedY = first.y + direction.1
-                if movedX < 0 || movedY < 0 || movedX > m - 1 || movedY > n - 1 {
-                    continue
-                }
+            for i in 0..<4 {
+                let newX = x + dx[i]
+                let newY = y + dy[i]
 
-                if !visited[movedY][movedX] && graph[movedY][movedX] == 1 {
-                    visited[movedY][movedX] = true
-                    queue.append((movedX, movedY))
+                if newX >= 0 && newX < cabbagePos[0].count && newY >= 0 && newY < cabbagePos.count {
+                    if cabbagePos[newY][newX] == 1 && !visited[newY][newX] {
+                        queue.append((newX, newY))
+                        visited[newY][newX] = true
+                    }
                 }
             }
         }
     }
 }
+
+boj_1012()
