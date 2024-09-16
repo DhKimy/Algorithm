d25670fe6acec18dca44efb2f44a60483cd4bd63
DongHyeon Kim(Duncan) <98330884+DhKimy@users.noreply.github.com>
2024-09-16 18:28:52 +0900

    [Silver I] Title: 나이트의 이동, Time: 44 ms, Memory: 75288 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.swift
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.swift
@@ -1,47 +1,47 @@
 func boj_7562() {
-        let t = Int(readLine()!)!
-        let dir = [(-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, 2), (1, -2), (2, 1), (2, -1)]
-        var result = ""
+    let t = Int(readLine()!)!
+    let dir = [(-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, 2), (1, -2), (2, 1), (2, -1)]
+    var result = ""
 
-        for _ in 0..<t {
-            let l = Int(readLine()!)!
-            let startPoint = readLine()!.split(separator: " ").map { Int($0)! }
-            let endPoint = readLine()!.split(separator: " ").map { Int($0)! }
-            var visited = [[Bool]](repeating: [Bool](repeating: false, count: l), count: l)
-            var queue = [(x: Int, y: Int, length: Int)]()
-            var minLength = 2058392010
+    for _ in 0..<t {
+        let l = Int(readLine()!)!
+        let startPoint = readLine()!.split(separator: " ").map { Int($0)! }
+        let endPoint = readLine()!.split(separator: " ").map { Int($0)! }
+        var visited = [[Bool]](repeating: [Bool](repeating: false, count: l), count: l)
+        var queue = [(x: Int, y: Int, length: Int)]()
+        var head = 0
 
-            if startPoint[0] == endPoint[0] && endPoint[1] == startPoint[1] {
-                result += "0\n"
-                continue
-            }
-
-            queue.append((startPoint[0], startPoint[1], 0))
-            visited[startPoint[1]][startPoint[0]] = true
-
-            while !queue.isEmpty {
-                let first = queue.removeFirst()
-
-                for i in dir {
-                    let newX = first.x + i.0
-                    let newY = first.y + i.1
-
-                    if newX >= 0 && newY >= 0 && newX < l && newY < l && !visited[newY][newX] {
-                        if newX == endPoint[0] && newY == endPoint[1] {
-                            minLength = min(minLength, first.length + 1)
-                            break
-                        }
-
-                        queue.append((newX, newY, first.length + 1))
-                        visited[newY][newX] = true
-                    }
-                }
-            }
-
-            result += "\(minLength)\n"
+        if startPoint[0] == endPoint[0] && endPoint[1] == startPoint[1] {
+            result += "0\n"
+            continue
         }
 
-        print(result)
+        queue.append((startPoint[0], startPoint[1], 0))
+        visited[startPoint[1]][startPoint[0]] = true
+
+        while !queue.isEmpty {
+            let first = queue[head]
+            head += 1
+
+            for i in dir {
+                let newX = first.x + i.0
+                let newY = first.y + i.1
+
+                if newX >= 0 && newY >= 0 && newX < l && newY < l && !visited[newY][newX] {
+                    if newX == endPoint[0] && newY == endPoint[1] {
+                        result += "\(first.length + 1)\n"
+                        queue.removeAll()
+                        break
+                    }
+
+                    queue.append((newX, newY, first.length + 1))
+                    visited[newY][newX] = true
+                }
+            }
+        }
     }
+
+    print(result)
+}
 
 boj_7562()
