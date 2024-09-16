a644c86c6215c7832d427904f42f3e8ccee0cb60
DongHyeon Kim(Duncan) <98330884+DhKimy@users.noreply.github.com>
2024-09-16 15:11:01 +0900

    [Silver I] Title: 미로 탐색, Time: 8 ms, Memory: 69252 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.swift
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.swift
@@ -1,38 +1,38 @@
-let firstLine = readLine()!.split(separator: " ").map { Int(String($0))! }
-let n = firstLine[0]
-let m = firstLine[1]
-var graph: [[Int]] = []
-var queue: [(x: Int, y: Int, count: Int)] = [(0, 0, 1)]
-var visited = [[Bool]](repeating: [Bool](repeating: false, count: m), count: n)
-let direct = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-for _ in 0..<n {
-    graph.append(readLine()!.map { Int(String($0))! })
-}
-var answer = 0
+func boj_2178() {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let (n, m) = (input[0], input[1])
+    let dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-visited[0][0] = true
+    var graph = [[Int]]()
+    var queue = [(x: Int, y: Int, length: Int)]()
+    var visited = [[Bool]](repeating: [Bool](repeating: false, count: m), count: n)
+    var length = 100000000
 
-while !queue.isEmpty {
-    let first = queue.removeFirst()
-
-    if first.x == m - 1 && first.y == n - 1 {
-        answer = first.count
-        break
+    for _ in 0..<n {
+        graph.append(readLine()!.map { Int(String($0))! })
     }
 
-    for i in direct {
-        let changedX = first.x + i.0
-        let changedY = first.y + i.1
+    queue.append((0, 0, 1))
+    visited[0][0] = true
 
-        if changedX < 0 || changedY < 0 || changedX > m - 1 || changedY > n - 1 {
-            continue
-        }
+    while !queue.isEmpty {
+        let point = queue.removeFirst()
 
-        if graph[changedY][changedX] == 1 && !visited[changedY][changedX] {
-            visited[changedY][changedX] = true
-            queue.append((changedX, changedY, first.count + 1))
+        for i in dir {
+            let newX = point.x + i.0
+            let newY = point.y + i.1
+
+            if newX >= 0 && newY >= 0 && newX < m && newY < n && !visited[newY][newX] && graph[newY][newX] == 1 {
+                queue.append((newX, newY, point.length + 1))
+                visited[newY][newX] = true
+            }
+
+            if newX == m - 1 && newY == n - 1 {
+                length = min(length, point.length + 1)
+            }
         }
     }
+    print(length)
 }
 
-print(answer)
+boj_2178()
