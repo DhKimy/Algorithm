f664de8c0e08e1ead5c0ba5c13f933bba3fe55c3
DongHyeon Kim(Duncan) <98330884+DhKimy@users.noreply.github.com>
2024-09-16 14:45:41 +0900

    [Silver I] Title: 그림, Time: 80 ms, Memory: 71288 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.swift
+++ 백준/Silver/1926. 그림/그림.swift
@@ -1,53 +1,55 @@
-let firstLine = readLine()!.split(separator: " ").map { Int($0)! }
-let n = firstLine[0]
-let m = firstLine[1]
-var graph: [[Int]] = []
-for _ in 0..<n {
-    graph.append(
-        readLine()!.split(separator: " ").map { Int($0)! }
-    )
-}
-let direct = [(0, -1), (0, 1), (-1, 0), (1, 0)] // 상하좌우
-var queue: [(x: Int, y: Int)] = []
-var visited = [[Bool]](repeating: [Bool](repeating: false, count: m), count: n)
-var count = 0
-var surface = 0
+func boj_1926() {
+    let input = readLine()!.split(separator: " ").map { Int($0)!}
+    let (m, n) = (input[1], input[0])
+    let dx = [-1, 1, 0, 0]
+    let dy = [0, 0, -1, 1]
+    var graph = [[Int]]()
+    var visited = [[Bool]](repeating: [Bool](repeating: false, count: m), count: n)
+    var queue = [(x: Int, y: Int)]()
+    var maxSurface = 0
+    var count = 0
 
-for y in 0..<n {
-    for x in 0..<m {
-        if graph[y][x] == 1 && visited[y][x] == false {
-            queue.append((x: x, y: y))
-            visited[y][x] = true
-            bfs()
-            count += 1
-        }
+    for _ in 0..<n {
+        graph.append(readLine()!.split(separator: " ").map { Int($0)! })
     }
-}
 
-func bfs() {
-    var innerSurface = 1
-    while !queue.isEmpty {
-        let element = queue.removeFirst()
-
-        for k in direct {
-            let changedX = element.x + k.0
-            let changedY = element.y + k.1
-
-            if changedX < 0 || changedX >= m || changedY < 0 || changedY >= n {
+    for y in 0..<n {
+        for x in 0..<m {
+            if visited[y][x] {
                 continue
             }
-
-            if !visited[changedY][changedX] && graph[changedY][changedX] == 1 {
-                visited[changedY][changedX] = true
-                innerSurface += 1
-                queue.append((changedX, changedY))
-            } else {
-                continue
+            if graph[y][x] == 1 {
+                count += 1
+                bfs(point: (x, y))
             }
         }
     }
-    surface = max(surface, innerSurface)
+    print(count)
+    print(maxSurface)
+
+    func bfs(point: (x: Int, y: Int)) {
+        var currentSurface = 1
+
+        queue.append(point)
+        visited[point.y][point.x] = true
+
+        while !queue.isEmpty {
+            let curPoint = queue.removeFirst()
+
+            for i in 0..<4 {
+                let newX = curPoint.x + dx[i]
+                let newY = curPoint.y + dy[i]
+
+                if newX >= 0 && newY >= 0 && newX < m && newY < n && !visited[newY][newX] && graph[newY][newX] == 1{
+                    queue.append((newX, newY))
+                    visited[newY][newX] = true
+                    currentSurface += 1
+                }
+            }
+        }
+
+        maxSurface = max(maxSurface, currentSurface)
+    }
 }
 
-print(count)
-print(surface)
+boj_1926()
